b6662e1a01f409e0986bb0a5e6af3666a559b7d4
kevinphyoe <95071207+kevinphyoe@users.noreply.github.com>
2026-07-18 22:48:21 -0700

    Update AMI lookup to use Amazon Linux 2023

--- main.tf
+++ main.tf
@@ -1,22 +1,24 @@
-data "aws_ami" "app_ami" {
+# 1. Lookup a current Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["al2023-ami-2023.*-x86_64"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.nano"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              dnf update -y
+              dnf install -y tomcat
+              systemctl enable --now tomcat
+              EOF
 
   tags = {
     Name = "HelloWorld"
